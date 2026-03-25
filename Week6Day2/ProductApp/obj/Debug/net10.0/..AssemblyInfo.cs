17bdae3b8605ee0ce54e2220547b641340a273be
ashishgupta74901@gmail.com <ashishgupta74901@gmail.com>
2026-03-25 12:22:22 +0530

    Added Week6Day2 and other changes

--- Week6Day2/ProductApp/obj/Debug/net10.0/..AssemblyInfo.cs
+++ Week6Day2/ProductApp/obj/Debug/net10.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c1d1be43216fe44ddff581649a05dd0ca87a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f252a29c60ba51a1fa9b514bb42489f7bc21de8f")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
